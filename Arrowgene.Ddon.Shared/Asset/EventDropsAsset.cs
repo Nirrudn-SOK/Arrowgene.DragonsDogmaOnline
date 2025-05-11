8f93e54af72c61c141497fb41e0016492c5cdac2
Nirrudn <57414492+Nirrudn-SOK@users.noreply.github.com>
2025-05-11 10:00:14 -0400

    Update EventDropsAsset.cs
    Add an Event field to support Event Items without a quest associated to them.

--- Arrowgene.Ddon.Shared/Asset/EventDropsAsset.cs
+++ Arrowgene.Ddon.Shared/Asset/EventDropsAsset.cs
@@ -34,6 +34,7 @@ namespace Arrowgene.Ddon.Shared.Asset
         public EventItemConstraint ItemConstraint { get; set; }
         public HashSet<uint> RequiredItemsEquipped { get; set; }
         public Species RequiredSpecies { get; set; }
+        public (string Key, uint Year) Event { get; set; }
     }
 
     public class EventDropsAsset
